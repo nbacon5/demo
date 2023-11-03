3f96d47bbc4172707f2aac038a7ec41307366942
nicholas <nicholas@flarzo.home>
2023-11-03 16:50:22 -0400

    new

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -3,10 +3,35 @@ package com.example;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.text.*;
+
 public class PrimaryController {
+    @FXML Text feedback;
+    @FXML TextField immigrantFirstName;
+    @FXML TextField immigrantLastName;
+    @FXML TextField immigrantAlienNumber;
+    @FXML TextField relativeFirstName;
+    @FXML TextField relativeLastName;
+    @FXML TextField relativeAlienNumber;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        if (immigrantFirstName.getText().equals("") ||
+            immigrantLastName.getText().equals("") ||
+            immigrantAlienNumber.getText().equals("") ||
+            relativeFirstName.getText().equals("") ||
+            relativeLastName.getText().equals("") ||
+            relativeAlienNumber.getText().equals("")){
+            
+            feedback.setText("One or more of the fields are empty");
+            return;
+        }
+        feedback.setText("Submitted");
     }
 }
